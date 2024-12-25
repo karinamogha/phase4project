d3ae596c4a0145ec500308bc59172d08476a8cb9
Ronald Roberts <djamadeus1@gmail.com>
2024-12-25 00:10:08 -0500

    Saving my Dec 24th current work before pulling

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import ExpenseForm from "./components/ExpenseForm";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavBar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
